463d8366dad9afcf051a7f491f28ea12893efc98
AngryPanda <924193579guge@gmail.com>
2019-12-09 18:18:12 +0800

    更换成更美观的样式

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let css1 = `/*
-*你好，这是我的个人简历。
+*你好，欢迎来到这里。
+*这里展示的是一个动画简历。
+*如果你也喜欢这种风格的简历，欢迎互相交流。
+*话不多说，动画开始。
 *先换个字体
 */
 *{
@@ -7,52 +10,75 @@ let css1 = `/*
     font-family: Consolas, Monaco, 'Andale Mono';
 }
 body{
-    /*给背景换个颜色*/
-    background: #f5f2f0;
     /*字不要靠边缘太近*/
     padding: 16px;
     overflow: hidden;
+    /*再给背景换个颜色*/
+    background: rgb(39,40,34);   
 }
 /*给代码加上高亮*/
-.token.comment{ color: slategray; }
+.token.comment{ color: white; }
 .token.selector{ color: #690; }
-.token.punctuation{ color: #999; }
+.token.punctuation{ color: white; }
 .token.property{ color: #905; }
-.token.string{ color: #690; }
-/* 加点呼吸灯*/
+.token.string{ color: rgb(230,219,103); }
+.token.function, .token.class-name { color: #DD4A68; }
+
+/*给注释换个颜色*/
+.token.comment{ color: rgb(136,132,111)}
+/*给字体改颜色*/
 #pre{
-    border: 1px solid #aaa;
+    color:rgb(102,217,239); 
+}
+
+/* 加个呼吸灯
+* 3D 效果
+*/
+#pre{
+    border: 1px solid rgb(205, 29, 73);
     width: 50%;
     max-height: 687px;
     animation: breath 1s infinite alternate-reverse;
     padding-left:10px;
     overflow: auto;
+    transform: perspective(3500px) rotateY(30deg);
     
 }
 /*现在开始制作简历*/
 /*先来一张白纸*/
 /*在右边新建一张白纸*/
 
-
+#paper{
+    overflow: hidden;
+}
 `
 
 let md = `# 自我介绍
-我叫 XXX
+我叫 华翔
 
 1997 年 12 月出生
 
-XXX 学校毕业
+热爱前端开发
 
-自学前端半年
+求知若渴
 
 希望应聘前端开发岗位
 
 # 技能介绍
-熟悉 JavaScript CSS
+- 熟练 HTML、CSS 的页面布局，能编写合理的 HTML 以及规范化的 CSS
+- 熟悉 HTML 5 及语义化，了解 Canvas 动画制作，掌握 CSS 3 动画、过渡效果等常用技术
+- 熟悉原生 JavaScript，会使用 ES6+ 常用规范，了解 jQuery 常用 API
+- 熟悉 Vue 常用功能，理解如生命周期、组件、双向绑定等功能
+- 熟悉小程序开发，理解如生命周期、组件等常用功能
+- 熟悉模块化、工程化开发流程，能够配置 Webpack
+- 有移动端开发经验，会使用REM、vw/ vh、响应式 等技术制作适配手机设备的页面
+- 了解 HTTP 相关知识，了解常见的 Web 性能优化方案
+- 熟练使用 VsCode、Sublime、Git、Http-Server 等开发工具
+
 # 项目介绍
-1. [苹果风格轮播](https://hx-angry-panda.github.io/appleSlideShow/index.html)
-2. [网页简历](https://hx-angry-panda.github.io/resume/index.html)
-3. [canvas 画板](https://hx-angry-panda.github.io/beginToCanvas/index.html)
+1. [Vue.js搭建技术社区](https://hx-angry-panda.github.io/copy-cnode/)
+2. [多语种翻译小程序](https://github.com/Hx-angry-panda/translate-miniprogram)
+3. [简易在线画图](https://hx-angry-panda.github.io/beginToCanvas/index.html)
 # 联系方式
 
  QQ xxxxxxxx
@@ -67,16 +93,28 @@ let css2 = `/*这个简历是用 Markdown 语法写的
 *转换中......
 *马上就好了
 */
-
 `
 
-let css3 = `/*简历的效果不是很好，改变一下它的样式吧*/
+let css3 = `/*把简历美化一下吧
+*换一个背景颜色
+*加上边框
+*同样加上呼吸灯
+*3D 效果接上
+*改变字体颜色
+*/
 `
 
 let css4 = `
 #paper{
-    max-height: 730px;
-    background: #f5f2f0;
+    max-height: 685px;
+    background: rgb(39,40,34);
+    margin: 16px;
+    border: 1px solid rgb(205, 29, 73);
+    animation: breath 1s infinite alternate-reverse;
+    transform: perspective(3500px) rotateY(-30deg);
+    color: rgb(205, 29, 73);
+    padding-left: 10px;
+    overflow: auto;
 }
 #paper pre ol{
     list-style: none;
@@ -91,7 +129,7 @@ let css4 = `
     margin-left: 10px;
 }
 #paper a{
-    text-decoration: none;
+    color: rgb(205, 29, 73);
 }
 #paper #联系方式 p{
     font-size: 18px;
@@ -113,7 +151,7 @@ function writeCss(prefix,code,fn){
             window.clearInterval(id)
             fn()
         }
-    }, 30)
+    }, 15)
 }
 
 function createPaper(fn) {
@@ -128,13 +166,13 @@ function writeMarkDown(code,fn){
     let n = 0
     var paperCode = document.querySelector('#paper > pre')
     let id = setInterval(function(){
-        paperCode.innerHTML = code.substring(0, n)
+        paperCode.innerHTML = code.substring(0, n)        
         n += 1
         if(n >= code.length){
             window.clearInterval(id)
             fn()
         }
-    },30)
+    },15)
 }
 
 function makeMarkDown(fn) {
